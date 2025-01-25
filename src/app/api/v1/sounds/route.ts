cc5c8583dc976dfe62ec61a8dbb8a5060cc783f3
ethanshealey <ethan.shealey@gmail.com>
2025-01-24 22:52:09 -0500

    safe netting

--- src/app/api/v1/sounds/route.ts
+++ src/app/api/v1/sounds/route.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { db } from '@/firebase'
 import { addDoc, collection, getDocs, query } from 'firebase/firestore'
+import { NextRequest } from 'next/server'
 
 export async function GET(request: Request) {
   const q = query(collection(db, 'sounds'))
@@ -11,8 +12,13 @@ export async function GET(request: Request) {
   return Response.json({ sounds })
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
+
   const body = await request.json()
+
+  console.log(request.url)
+
+  if(!request.url.includes('https://soundboard.ethanshealey.com')) return Response.json({ message: 'Invalid request' }, { status: 400 })
 
   await addDoc(collection(db, 'sounds'), {
     name: body.name,
